2abe62ce4c4f92d97bcb5061de29d25dd3535bfb
najarani <nirajanpoudel77@gmail.com>
2023-08-15 22:12:16 +0545

    finishing of selection sorting with example

--- sortingnlast.java
+++ sortingnlast.java
@@ -1,5 +1,25 @@
 public class sortingnlast{
+public static void printArray(int arr[]){
+    for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+}
 public static void main(String[] args) {
-    System.out.println("Hello World");
+//selection sort
+int arr[] = {7,8,3,1,2};
+for (int i=0; i<arr.length-1; i++){
+    int smallest =i;
+    for(int j = i+1; j<arr.length; j++){
+        if(arr[j]<arr[smallest]){
+            smallest = j;
+        }
+    }
+    int temp = arr[smallest];
+    arr[smallest] = arr[i];
+    arr[i] = temp;
+}
+printArray(arr);
+
 }
 }
