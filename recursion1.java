84e31e04459c440b9973159e959977ad96bd3685
najarani <nirajanpoudel77@gmail.com>
2023-08-16 14:10:00 +0545

    recursion first push

---
+++ recursion1.java
@@ -0,0 +1,25 @@
+public class recursion1 {
+    //print n numbers
+public static void printNumb(int n){
+    if (n==0){
+        return;
+    }
+     System.out.println(n); 
+    printNumb(n-1);
+   
+}
+//print sum of first n natiural numbers 
+public static void printSum(int i, int n, int sum){
+    if (i==n){
+        sum=sum+i;
+        System.out.println(sum);
+        return;
+    }
+    sum=sum+i;
+    printSum(i+1, n, sum);
+}
+    public static void main(String[] args) {
+        printSum(1, 5, 0);
+        
+    }
+}
