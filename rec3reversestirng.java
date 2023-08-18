1d40b68338adbc3903cc3e8013503e9588997a3b
najarani <nirajanpoudel77@gmail.com>
2023-08-18 22:00:49 +0545

    second string recursion question solved

--- rec3reversestirng.java
+++ rec3reversestirng.java
@@ -7,9 +7,24 @@ public class rec3reversestirng {
         System.out.print(s.charAt(in));
         reversstring(s, in-1);
     }
+    public static void firstandlaststring( int index, String str){
+        if(index==str.length()-1){
+            System.out.println("last string is " + str.charAt(index));
+            return;
+        }
+        if(index==0){
+         System.out.println("first string is " +str.charAt(0));
+        }
+        
+        firstandlaststring(index+1, str);
+
+    }
+    //find the 1st and last occurance of an element in string
+
+
     public static void main(String[] args) {
-        String s = "abcd";
-        reversstring(s, s.length()-1);
+        String str = "abcd";
+        firstandlaststring(0, str);
     }
 
     
