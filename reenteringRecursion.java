60948873cb44ac7720836b516b14bfa080008132
najarani <nirajanpoudel77@gmail.com>
2023-08-23 21:27:52 +0545

    wrapping for today

---
+++ reenteringRecursion.java
@@ -0,0 +1,17 @@
+public class reenteringRecursion {
+    //print all the subsequences of a stirng in java
+    public static void subsequences(String str, int idx, String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currchar = str.charAt(idx);
+
+        subsequences(str, idx+1, newString+currchar);
+        subsequences(str, idx+1, newString);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        subsequences(str, 0, "");
+    }
+}
