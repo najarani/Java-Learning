fb03e138695af3b68e61e9dc31695d4504d68a9d
najarani <nirajanpoudel77@gmail.com>
2024-01-16 16:48:05 +0545

    backtotrack

---
+++ letslearnqueues.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class letslearnqueues {
+    public static void main(String[] args) {
+       //Ques is first in first out
+       //Linear data structure
+       Queue<String> queue = new LinkedList<String>(null);
+
+
+    }
+}
